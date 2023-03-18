ae9d8377e16d0526ea5c9a50284dc222aeeaa62a
Another <89122911+hexvel@users.noreply.github.com>
2023-03-18 12:10:31 +0300

    Add files via upload

---
+++ src/Containers/SectionArticles.jsx
@@ -0,0 +1,43 @@
+function Articles() {
+    return (
+        <section className="SectionArticles">
+            <p className="TextArticles">
+                Articles
+            </p>
+            <div className="ArticleContainer">
+                <div className="ArticlesItems">
+                    <div className="Cards">
+                        <img src="/images/Rectangle-18.jpg"/>
+                        <div className="ArticleItemProps">
+                            <p className="time">15 Now 2021</p>
+                            <h3 className="names">Juridicrion</h3>
+                            <p className="Lionic">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
+                        </div>
+                        <img src="/images/Rectangle-20.jpg"/>
+                        <div className="ArticleItemProps">
+                            <p className="time">15 Now 2021</p>
+                            <h3 className="names">Consulting</h3>
+                            <p className="Lionic">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
+                        </div>
+                    </div>
+                    <div className="Cards">
+                        <img src="/images/Rectangle-23.jpg"/>
+                        <div className="ArticleItemProps">
+                            <p className="time">15 Now 2021</p>
+                            <h3 className="names">Rights and obligations</h3>
+                            <p className="Lionic">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
+                        </div>
+                        <img src="/images/Rectangle-24.jpg"/>
+                        <div className="ArticleItemProps">
+                            <p className="time">15 Now 2021</p>
+                            <h3 className="names">Contractors</h3>
+                            <p className="Lionic">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Articles;
---
+++ src/Containers/SectionClients.jsx
@@ -0,0 +1,69 @@
+function Clients() {
+    return (
+        <section className="SectionClients">
+            <p className="TextArticles">
+                Clients
+            </p>
+            <div className="ClientItems">
+                <div className="ClientContainer">
+                    <div className="ClientCards">
+                        <div className="textClient ClientText">
+                            <p className="Lionic">
+                                Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum dolore eu feugiat nulla facilisis at vero eros et accumsan et
+                                iusto odio dignissim qui blandit odio dignissim qui blandit molestie consequat.
+                            </p>
+                        </div>
+                        <div className="authorClient">
+                            <div className="authorClientName">
+                                <img className="imageAuthor" src="/images/group_43.png"/>
+                                <div className="Clientjob">
+                                    <h4 className="Names">John Armstrong</h4>
+                                    <p>Customer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ClientCards">
+                        <div className="textClient ClientText">
+                            <p className="Lionic">
+                                Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum dolore eu feugiat nulla facilisis at vero eros et accumsan et
+                                iusto odio dignissim qui blandit odio dignissim qui blandit molestie consequat.
+                            </p>
+                        </div>
+                        <div className="authorClient">
+                            <div className="authorClientName">
+                                <img className="imageAuthor" src="/images/group_42.png"/>
+                                <div className="Clientjob">
+                                    <h4 className="Names">Elizabeth Martin</h4>
+                                    <p>Customer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ClientCards">
+                        <div className="textClient ClientText">
+                            <p className="Lionic">
+                                Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum dolore eu feugiat nulla facilisis at vero eros et accumsan et
+                                iusto odio dignissim qui blandit odio dignissim qui blandit molestie consequat.
+                            </p>
+                        </div>
+                        <div className="authorClient">
+                            <div className="authorClientName">
+                                <img className="imageAuthor" src="/images/group_72.png"/>
+                                <div className="Clientjob">
+                                    <h4 className="Names">Robin Soutby</h4>
+                                    <p>Customer</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Clients;
